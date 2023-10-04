9d8129ff40bf056fa7b8235b5e7672821a56ce03
xxxuuu <x751736277@gmail.com>
2023-10-04 12:53:37 +0800

    update mappings

--- lua/astronvim/mappings.lua
+++ lua/astronvim/mappings.lua
@@ -341,7 +341,7 @@ if is_available "toggleterm.nvim" then
     maps.n["<leader>tn"] = { function() utils.toggle_term_cmd "node" end, desc = "ToggleTerm node" }
   end
   if vim.fn.executable "gdu" == 1 then
-    maps.n["<leader>tu"] = { function() utils.toggle_term_cmd "gdu" end, desc = "ToggleTerm gdu" }
+    maps.n["<leader>tu"] = { function() utilt.toggle_term_cmd "gdu" end, desc = "ToggleTerm gdu" }
   end
   if vim.fn.executable "btm" == 1 then
     maps.n["<leader>tt"] = { function() utils.toggle_term_cmd "btm" end, desc = "ToggleTerm btm" }
@@ -349,12 +349,14 @@ if is_available "toggleterm.nvim" then
   local python = vim.fn.executable "python" == 1 and "python" or vim.fn.executable "python3" == 1 and "python3"
   if python then maps.n["<leader>tp"] = { function() utils.toggle_term_cmd(python) end, desc = "ToggleTerm python" } end
   maps.n["<leader>tf"] = { "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm float" }
-  maps.n["<leader>th"] = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "ToggleTerm horizontal split" }
+  maps.n["<leader>th"] = { "<cmd>ToggleTerm size=20 direction=horizontal<cr>", desc = "ToggleTerm horizontal split" }
+  maps.n["tt"] = maps.n["<leader>th"]
   maps.n["<leader>tv"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "ToggleTerm vertical split" }
   maps.n["<F7>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" }
   maps.t["<F7>"] = maps.n["<F7>"]
   maps.n["<C-'>"] = maps.n["<F7>"] -- requires terminal that supports binding <C-'>
   maps.t["<C-'>"] = maps.n["<F7>"] -- requires terminal that supports binding <C-'>
+  maps.t["<esc>"] = { "<C-\\><C-n>", desc = "Leave terminal" }
 end
 
 if is_available "nvim-dap" then
